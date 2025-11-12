1de81048e94ec85bc92dcb58e7d4374ffe2155db
Leonardo Burbano <br.david@outlook.com>
2025-11-11 23:27:55 -0500

    limpieza archivos proyecto anterior

--- app/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer container">
-      © {new Date().getFullYear()} Volt&Craft Studio · Cali, Colombia · contacto@tudominio.com
-    </footer>
-  );
-}
